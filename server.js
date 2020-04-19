f446dd1f3ce09f19947544e313a7335061e4bce1
meteoratlas <kellyaccnts@gmail.com>
2020-04-18 22:22:53 -0600

    added mongoose

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
-const port = 3000;
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.MONGODD_PASS);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connection successful.');
+  });
+
 app.listen(process.env.PORT, () => {
   console.log('App running on port', process.env.PORT);
 });
