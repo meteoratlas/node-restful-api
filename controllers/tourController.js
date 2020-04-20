509232eec35bdb688b1729e4aeb5fccf13051288
meteoratlas <kellyaccnts@gmail.com>
2020-04-19 23:31:30 -0600

    tour controller result filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,20 @@ const Tour = require('./../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // build query
+    const queryObj = { ...req.query };
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((e) => delete queryObj[e]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = await Tour.find(JSON.parse(queryStr));
+
+    // execute query
+    const tours = await query;
+
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
