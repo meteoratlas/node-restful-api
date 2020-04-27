84793edeae07d88a2bcbed5a594d2d7b8e20ff6b
meteoratlas <kellyaccnts@gmail.com>
2020-04-26 23:16:26 -0600

    user cookies, rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const errorHandler = require('./controllers/errorController');
@@ -12,6 +13,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  // 100 reqs per hour
+  message: 'Too many requests from this IP; please try again later.',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
--- controllers/authController.js
+++ controllers/authController.js
@@ -207,6 +207,20 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    //secure: true, // unneeded in dev
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // remove password from the output (doesn't delete password!)
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
