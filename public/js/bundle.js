91d685c8e0e0c31cf3e186c55a65095a63985eba
meteoratlas <kellyaccnts@gmail.com>
2020-05-14 23:00:30 -0600

    client side image uploading

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8872,12 +8872,12 @@ if (logOutButton) {
 if (accountUpdateSubmitBtn) {
   accountUpdateSubmitBtn.addEventListener('submit', function (e) {
     e.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    (0, _updateSettings.updateData)({
-      name: name,
-      email: email
-    }, 'data');
+    var form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    console.log(form);
+    (0, _updateSettings.updateData)(form, 'data');
   });
 }
 
@@ -8948,7 +8948,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54363" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54228" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
