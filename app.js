745a55100d6eb50915392951d69a746cc585533c
meteoratlas <kellyaccnts@gmail.com>
2020-05-20 22:11:58 -0600

    booking route, controller

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const errorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingsRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -77,6 +78,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingsRouter);
 
 app.all('*', (req, res, next) => {
   // passing an argument to next() always tells express there was an error
